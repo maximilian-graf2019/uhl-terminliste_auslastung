2467566628aaa52dc24ce85d813fd273aee39ec0
Maximilian Graf <mail@grafmaximilian.com>
2022-12-08 20:24:30 +0100

    added new pdf output method

--- grafiken.py
+++ grafiken.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import pathlib
 from md2pdf.core import md2pdf
+from fpdf import FPDF
 
 excel_files = glob.glob("*.xlsx")
 file = excel_files[0]
@@ -165,8 +166,40 @@ file_to_rem.unlink()
 for abt in arbeitsbereiche:
     update_markdown(filename_output_markdown, abt)
 
+title = 'Übersicht Fertigung Auslastung + Kapazität'
 
-pdf_output_path = f'Fertigungsübersicht_KW{today.isocalendar().week}.pdf'
 
-md2pdf(pdf_file_path=pdf_output_path,
-       md_file_path=filename_output_markdown)
+class PDF(FPDF):
+    def header(self):
+        # Arial bold 15
+        self.set_font('Arial', 'B', 15)
+        # Calculate width of title and position
+        w = self.get_string_width(title) + 6
+        self.set_x((210 - w) / 2)
+        # Colors of frame, background and text
+        self.set_draw_color(0, 80, 180)
+        self.set_fill_color(230, 230, 0)
+        self.set_text_color(220, 50, 50)
+        # Thickness of frame (1 mm)
+        self.set_line_width(1)
+        # Title
+        self.cell(w, 9, title, 1, 1, 'C', 1)
+        # Line break
+        self.ln(10)
+
+    def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Text color in gray
+        self.set_text_color(128)
+        # Page number
+        self.cell(0, 10, 'Seite ' + str(self.page_no()), 0, 0, 'C')
+
+
+
+pdf = PDF()
+pdf.set_title(title)
+pdf.set_author('Maximilian Graf')
+pdf.output('fertigung.pdf', 'F')
